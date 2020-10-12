ec1b968fd9afc2e630e86ad1148d945c5df4b30c
fodare <folorunsodamilare007@hotmail.com>
2020-10-12 07:01:05 +0300

    Started the Get requests and connected to the open weather external api and parsed the data. to work on the Post request

---
+++ Weather-Project/app.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const https = require('https');
+const app = express();
+
+app.listen(3000, function(){
+  console.log("Server running on port 3000");
+});
+
+// Home Page
+// GET
+app.get("/", function(req, res){
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Tallinn&appid=9fac45b09a2608590fb6c4e5a746dbbe&units=metric";
+  https.get(url, function(response){
+    console.log(response.statusCode);
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const iconDisplay = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/"+ iconDisplay +"@2x.png"
+      res.write("<p>The Weather is currently " + weatherDescription + "</p>");
+      res.write("<h1>The temperature in Tallinn is " + temp + " degress  Celcius.</h1>");
+      res.write("<img src="+ imageURL+ ">");
+      res.send();
+      console.log(temp);
+      console.log(weatherDescription);
+      console.log(iconDisplay);
+    } );
+  });
+});
