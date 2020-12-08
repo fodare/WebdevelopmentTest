269fdfd94a72f2778e562cbdcec1465553cd43f8
fodare <folorunsodamilare007@hotmail.com>
2020-12-08 07:39:38 +0200

    Introduction to node.js and npm package usage

---
+++ intro-to-Node/index.js
@@ -0,0 +1,17 @@
+// Node.js file system
+// const fs = require("fs");
+
+// // fs.copyFileSync("file.txt", "file1.txt");
+
+// Imported npm manager
+
+// require npm spureheroes
+var superheroes = require("superheroes");
+// require npm package supervillians
+var supervillains = require("supervillains");
+
+var my_super_hero_name = superheroes.random();
+console.log(`Superhero name is ${my_super_hero_name}`);
+
+var superVillianName = supervillains.random();
+console.log(`Supervillian name is ${superVillianName}`);
