264f7bc21d1a1c92b4e3e3352c38c96e808ead16
fodare <folorunsodamilare007@hotmail.com>
2021-01-23 16:21:59 +0100

    Complete API Path, Authntication and data Parsing

--- API_Practice/app.js
+++ API_Practice/app.js
@@ -1,20 +1,47 @@
 const express = require("express");
+const { write } = require("fs");
+const bodyParser = require("body-parser");
 const https = require("https"); // For making a https request
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
+   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+   const query = req.body.cityName;
+   const appkey = "299f3fff0394893846ae1fe3432ca3f7";
+   const mearsurement = "metric";
    const url =
-      "https://newsapi.org/v2/top-headlines?country=de&pageSize=20&apikey=035eeec1dcf04686baf46df318753c67";
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      query +
+      "&appid=" +
+      appkey +
+      "&units=" +
+      mearsurement;
    https.get(url, function (response) {
       console.log(response.statusCode);
       response.on("data", function (data) {
-         newsData = JSON.parse(data);
-         console.log(newsData);
+         weather_data = JSON.parse(data);
+         temp = weather_data.main.temp;
+         weatherDescription = weather_data.weather[0].description;
+         const weatherIcon = weather_data.weather[0].icon;
+         const imageURL =
+            "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+
+         res.write(
+            `<h1>The weather description in ${query} is: ${weatherDescription}</h1>`
+         );
+         res.write(
+            `<h1>The temprature in ${query}, DE is ${temp} degree(s)</h1>`
+         );
+
+         res.write("<img src= " + imageURL + ">");
+         res.send();
       });
    });
-
-   res.send("Server up and running");
 });
 
 app.listen(3000, function () {
