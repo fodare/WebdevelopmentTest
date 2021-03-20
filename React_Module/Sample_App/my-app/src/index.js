6dd7ab2a865b298eb56f7220aaa5489a6cadaec3
fodare <folorunsodamilare007@hotmail.com>
2021-03-20 21:31:26 +0100

    Begining of React lessons on React components

---
+++ React_Module/Sample_App/my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+   <div>
+      <h1>My name is Damilare!</h1>
+      <p>This is my first react app</p>
+   </div>,
+   document.getElementById("root")
+);
---
+++ React_Module/keeper-app-part-1-starting-code/src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+   return (
+      <div className="note">
+         <h1>This is the note title.</h1>
+         <br></br>
+         <p>This is the content</p>
+      </div>
+   );
+}
+
+export default Note;
